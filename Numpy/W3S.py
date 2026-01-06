eca6773d4cc68110ff883cb4b5e816aa60861884
amargauge26 <amardeep.singh0755@gmail.com>
2026-01-07 00:10:37 +0530

    Updated data preprocessing logic

--- Numpy/W3S.py
+++ Numpy/W3S.py
@@ -22,3 +22,21 @@ print(d.ndim)
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 
 print('2nd element on 1st row: ', arr[0, 1])
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[-3:-1])
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+print(arr[1, 1:4])
+
+
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4], dtype='S')
+
+print(arr)
+print(arr.dtype)
+
+
